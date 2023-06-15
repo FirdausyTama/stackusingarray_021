3254e918e3355b9acc8b722a29310c085a5d1373
FirdausyTama <atama.firdausy@gmail.com>
2023-06-15 11:03:19 +0700

    jika memilih menu nomer 1 akan di suruh masukan element yg mau dimasukan

--- stackusingarray/stackusingarray.cpp
+++ stackusingarray/stackusingarray.cpp
@@ -68,5 +68,15 @@ int main() {
 		string input;
 		getline(cin, input);
 		char ch = (input.empty() ? '0' : input[0]);
+		switch(ch) {
+		case '1': {
+			cout << "\nEnter an elemnt: ";
+			string element;
+			getline(cin, element);
+			s.push(element);
+			break;
+		}
+		
+		}
 	}
 }
